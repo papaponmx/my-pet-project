7b5bd38a8ca416c26d637acc3b550a2662229e6b
Jaime Rios <jaime.rios.dev@outlook.com>
2020-07-13 20:14:15 -0500

    My very first commit

---
+++ index.js
@@ -0,0 +1,337 @@
+const API_SHIP_RESPONSE = [
+    {
+      "ship_id": "AMERICANCHAMPION",
+      "ship_name": "American Champion",
+      "ship_model": null,
+      "ship_type": "Tug",
+      "roles": [
+        "Support Ship",
+        "Barge Tug"
+      ],
+      "active": false,
+      "imo": 7434016,
+      "mmsi": 367020820,
+      "abs": 571252,
+      "class": 7604342,
+      "weight_lbs": 588000,
+      "weight_kg": 266712,
+      "year_built": 1976,
+      "home_port": "Port of Los Angeles",
+      "status": "Stopped",
+      "speed_kn": 0,
+      "course_deg": null,
+      "position": {
+        "latitude": 30.52731,
+        "longitude": -88.10171
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [
+        {
+          "name": "COTS 1",
+          "flight": 7
+        },
+        {
+          "name": "COTS 2",
+          "flight": 8
+        }
+      ],
+      "url": "https://www.marinetraffic.com/en/ais/details/ships/shipid:434663/vessel:AMERICAN%20CHAMPION",
+      "image": "https://i.imgur.com/woCxpkj.jpg"
+    },
+    {
+      "ship_id": "AMERICANISLANDER",
+      "ship_name": "American Islander",
+      "ship_model": null,
+      "ship_type": "Cargo",
+      "roles": [
+        "Dragon Recovery"
+      ],
+      "active": false,
+      "imo": null,
+      "mmsi": 367035570,
+      "abs": null,
+      "class": null,
+      "weight_lbs": null,
+      "weight_kg": null,
+      "year_built": null,
+      "home_port": "Port of Los Angeles",
+      "status": "Stopped",
+      "speed_kn": 0,
+      "course_deg": null,
+      "position": {
+        "latitude": 33.75565,
+        "longitude": -118.1144
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [
+        {
+          "name": "CRS-1",
+          "flight": 9
+        },
+        {
+          "name": "CRS-2",
+          "flight": 10
+        },
+        {
+          "name": "CRS-3",
+          "flight": 14
+        },
+        {
+          "name": "CRS-4",
+          "flight": 18
+        }
+      ],
+      "url": "https://www.marinetraffic.com/en/ais/details/ships/shipid:435112/vessel:AMERICAN%20ISLANDER",
+      "image": "https://i.imgur.com/jmj8Sh2.jpg"
+    },
+    {
+      "ship_id": "AMERICANSPIRIT",
+      "ship_name": "American Spirit",
+      "ship_model": null,
+      "ship_type": "Cargo",
+      "roles": [
+        "Support Ship"
+      ],
+      "active": false,
+      "imo": null,
+      "mmsi": null,
+      "abs": null,
+      "class": null,
+      "weight_lbs": null,
+      "weight_kg": null,
+      "year_built": null,
+      "home_port": "Port of Los Angeles",
+      "status": null,
+      "speed_kn": null,
+      "course_deg": null,
+      "position": {
+        "latitude": null,
+        "longitude": null
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [
+        {
+          "name": "CASSIOPE",
+          "flight": 11
+        }
+      ],
+      "url": null,
+      "image": null
+    },
+    {
+      "ship_id": "ASOG",
+      "ship_name": "A Shortfall of Gravitas",
+      "ship_model": null,
+      "ship_type": "Barge",
+      "roles": [
+        "ASDS barge"
+      ],
+      "active": false,
+      "imo": null,
+      "abs": null,
+      "mmsi": null,
+      "class": null,
+      "weight_lbs": null,
+      "weight_kg": null,
+      "year_built": null,
+      "home_port": "Port Canaveral",
+      "status": null,
+      "speed_kn": null,
+      "course_deg": null,
+      "position": {
+        "latitude": null,
+        "longitude": null
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [],
+      "url": null,
+      "image": null
+    },
+    {
+      "ship_id": "BETTYRGAMBARELLA",
+      "ship_name": "Betty R Gambarella",
+      "ship_model": null,
+      "ship_type": "Tug",
+      "roles": [
+        "ASDS Tug"
+      ],
+      "active": false,
+      "imo": 7517478,
+      "mmsi": 368000890,
+      "abs": 562590,
+      "class": 7427463,
+      "weight_lbs": 446000,
+      "weight_kg": 202302,
+      "year_built": 1974,
+      "home_port": "Port of Los Angeles",
+      "status": "Towing Astern",
+      "speed_kn": 0,
+      "course_deg": null,
+      "position": {
+        "latitude": 33.77161,
+        "longitude": -118.2121
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [
+        {
+          "name": "FormoSat-5",
+          "flight": 46
+        },
+        {
+          "name": "Iridium NEXT Mission 3",
+          "flight": 48
+        }
+      ],
+      "url": "https://www.marinetraffic.com/en/ais/details/ships/shipid:428415/vessel:BETTY%20R%20GAMBARELLA",
+      "image": "https://i.imgur.com/ngYgFnn.jpg"
+    },
+    {
+      "ship_id": "ELSBETH3",
+      "ship_name": "Elsbeth III",
+      "ship_model": null,
+      "ship_type": "Tug",
+      "roles": [
+        "ASDS Tug"
+      ],
+      "active": false,
+      "imo": 8983375,
+      "mmsi": 367017460,
+      "abs": null,
+      "class": null,
+      "weight_lbs": 602000,
+      "weight_kg": 273063,
+      "year_built": 1999,
+      "home_port": "Port Canaveral",
+      "status": "Underway Using Engine",
+      "speed_kn": 9,
+      "course_deg": 57,
+      "position": {
+        "latitude": 25.98623,
+        "longitude": -97.3115
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [
+        {
+          "name": "CRS-5",
+          "flight": 19
+        },
+        {
+          "name": "DSCOVR",
+          "flight": 20
+        },
+        {
+          "name": "CRS-6",
+          "flight": 22
+        },
+        {
+          "name": "CRS-7",
+          "flight": 24
+        },
+        {
+          "name": "SES-9",
+          "flight": 27
+        },
+        {
+          "name": "CRS-8",
+          "flight": 28
+        },
+        {
+          "name": "JCSAT-2B",
+          "flight": 29
+        },
+        {
+          "name": "Thaicom 8",
+          "flight": 30
+        },
+        {
+          "name": "ABS-2A / Eutelsat 117W B",
+          "flight": 31
+        },
+        {
+          "name": "CRS-9",
+          "flight": 32
+        },
+        {
+          "name": "JCSAT-16",
+          "flight": 33
+        },
+        {
+          "name": "SES-10",
+          "flight": 38
+        },
+        {
+          "name": "BulgariaSat-1",
+          "flight": 42
+        },
+        {
+          "name": "Boeing X-37B OTV 5",
+          "flight": 47
+        }
+      ],
+      "url": "https://www.marinetraffic.com/en/ais/details/ships/shipid:434560/vessel:ELSBETH%20III",
+      "image": "https://i.imgur.com/1s5DUbJ.jpg"
+    },
+    {
+      "ship_id": "GONAVIGATOR",
+      "ship_name": "GO Navigator",
+      "ship_model": null,
+      "ship_type": "Cargo",
+      "roles": [
+        "Support Ship",
+        "Fairing Recovery"
+      ],
+      "active": true,
+      "imo": 9566887,
+      "mmsi": 367550000,
+      "abs": 1221524,
+      "class": 9204358,
+      "weight_lbs": 996000,
+      "weight_kg": 451778,
+      "year_built": 2009,
+      "home_port": "Port Canaveral",
+      "status": "Moored",
+      "speed_kn": 0,
+      "course_deg": null,
+      "position": {
+        "latitude": 28.41353,
+        "longitude": -80.62119
+      },
+      "successful_landings": null,
+      "attempted_landings": null,
+      "missions": [],
+      "url": "https://www.marinetraffic.com/en/ais/details/ships/shipid:452540/vessel:GO%20NAVIGATOR",
+      "image": "https://i.imgur.com/MjNWzhO.jpg"
+    }
+  ];
+// for (const letter of array1) {
+//   console.log(letter);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const listItem = document.querySelector('ul');
+
+const renderListItems = API_SHIP_RESPONSE.map(ship => {
+    return `<li>${ship.ship_name.toLowerCase()}</li>`;
+});
+
+listItem.appendChild(renderListItems());
